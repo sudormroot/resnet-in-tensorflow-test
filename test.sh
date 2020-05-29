b8cbd82e8f793234cb50250819d9cb82ed8dd990
sudormroot <sudormroot@163.com>
2020-05-29 15:56:29 +0800

    fix

--- test.sh
+++ test.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 
-stdbuf -oL python train.py --train_batch_size=128 --train_steps=12 2>&1 | tee -a output_batchsize-128_umem-no.txt
-stdbuf -oL python train.py --train_batch_size=256 --train_steps=12 2>&1 | tee -a output_batchsize-256_umem-no.txt
+script_path="`dirname $0`"
+
+source $script_path/env.sh
+
+stdbuf -oL python $script_path/train.py --train_batch_size=128 --train_steps=12 2>&1 | tee -a $script_path/output_batchsize-128_umem-no.txt
+stdbuf -oL python $script_path/train.py --train_batch_size=256 --train_steps=12 2>&1 | tee -a $script_path/output_batchsize-256_umem-no.txt

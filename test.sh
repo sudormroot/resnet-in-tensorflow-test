a104a78062fdb49387b0f086e13afe3ff6581cad
sudormroot <sudormroot@163.com>
2020-05-29 15:54:38 +0800

    add

--- test.sh
+++ test.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
-python cifar10_train.py --train_batch_size=128 --train_steps=2
+stdbuf -oL python train.py --train_batch_size=128 --train_steps=12 2>&1 | tee -a output_batchsize-128_umem-no.txt
+stdbuf -oL python train.py --train_batch_size=256 --train_steps=12 2>&1 | tee -a output_batchsize-256_umem-no.txt

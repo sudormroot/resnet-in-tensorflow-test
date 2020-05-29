17561fe88226e7cfd4698107e0b516d2f8409ebe
sudormroot <sudormroot@163.com>
2020-05-29 16:15:10 +0800

    add

--- test.sh
+++ test.sh
@@ -5,9 +5,9 @@ script_path="`dirname $0`"
 . $script_path/env.sh
 
 export UNIFIED_MEMORY_SET=no
-stdbuf -oL python $script_path/train.py --train_batch_size=128 --train_steps=12 2>&1 | tee -a $script_path/output_batchsize-128_umem-$UNIFIED_MEMORY_SET.txt
-stdbuf -oL python $script_path/train.py --train_batch_size=256 --train_steps=12 2>&1 | tee -a $script_path/output_batchsize-256_umem-$UNIFIED_MEMORY_SET.txt
+stdbuf -oL python $script_path/train.py --train_batch_size=128 --train_steps=12 2>&1 | tee $script_path/output_batchsize-128_umem-$UNIFIED_MEMORY_SET.txt
+stdbuf -oL python $script_path/train.py --train_batch_size=256 --train_steps=12 2>&1 | tee $script_path/output_batchsize-256_umem-$UNIFIED_MEMORY_SET.txt
 
 export UNIFIED_MEMORY_SET=yes
-stdbuf -oL python $script_path/train.py --train_batch_size=128 --train_steps=12 2>&1 | tee -a $script_path/output_batchsize-128_umem-$UNIFIED_MEMORY_SET.txt
-stdbuf -oL python $script_path/train.py --train_batch_size=256 --train_steps=12 2>&1 | tee -a $script_path/output_batchsize-256_umem-$UNIFIED_MEMORY_SET.txt
+stdbuf -oL python $script_path/train.py --train_batch_size=128 --train_steps=12 2>&1 | tee $script_path/output_batchsize-128_umem-$UNIFIED_MEMORY_SET.txt
+stdbuf -oL python $script_path/train.py --train_batch_size=256 --train_steps=12 2>&1 | tee $script_path/output_batchsize-256_umem-$UNIFIED_MEMORY_SET.txt

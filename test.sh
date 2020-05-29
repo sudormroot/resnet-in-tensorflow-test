9cff79d1934d6afaaeb4f3fa670a9f5545762e57
sudormroot <sudormroot@163.com>
2020-05-29 17:12:38 +0800

    add

--- test.sh
+++ test.sh
@@ -4,6 +4,8 @@ script_path="`dirname $0`"
 
 . $script_path/env.sh
 
+export CUDA_VISIBLE_DEVICES=1
+
 export UNIFIED_MEMORY_SET=no
 stdbuf -oL python $script_path/train.py --train_batch_size=128 --train_steps=12 2>&1 | tee $script_path/output_batchsize-128_umem-$UNIFIED_MEMORY_SET.txt
 stdbuf -oL python $script_path/train.py --train_batch_size=256 --train_steps=12 2>&1 | tee $script_path/output_batchsize-256_umem-$UNIFIED_MEMORY_SET.txt

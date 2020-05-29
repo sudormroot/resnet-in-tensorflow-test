c33921c56c64e2588486e77ca3b175c3da1d5a2f
sudormroot <sudormroot@163.com>
2020-05-29 15:57:46 +0800

    fix

--- test.sh
+++ test.sh
@@ -2,7 +2,7 @@
 
 script_path="`dirname $0`"
 
-source $script_path/env.sh
+. $script_path/env.sh
 
 stdbuf -oL python $script_path/train.py --train_batch_size=128 --train_steps=12 2>&1 | tee -a $script_path/output_batchsize-128_umem-no.txt
 stdbuf -oL python $script_path/train.py --train_batch_size=256 --train_steps=12 2>&1 | tee -a $script_path/output_batchsize-256_umem-no.txt
